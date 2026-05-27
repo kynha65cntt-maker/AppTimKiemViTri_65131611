5e1ac97613219b0e63fc2f4cdd380b2231c1a0bf
An Kỳ <ky.nha.65cntt@ntu.edu.vn>
2026-05-27 11:40:12 +0700

    Commit 3

---
+++ app/src/main/java/anky/ntu/edu/duanck_apptimkiemvitri_65131611/LoginActivity.java
@@ -0,0 +1,46 @@
+package anky.ntu.edu.duanck_apptimkiemvitri_65131611;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText emailEditText;
+    Button btnLogin;
+    FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        mAuth = FirebaseAuth.getInstance();
+        btnLogin.setOnClickListener(v -> loginUser());
+    }
+    private void loginUser() {
+        String userEmail = emailEditText.getText().toString();
+        String userPass = password.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(userEmail, userPass)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(this, "Login success", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(this, "Login failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+}
